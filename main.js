0f64092647c72981a014539725d1ef6dc739b416
subash-bay20 <107981942+subash-bay20@users.noreply.github.com>
2022-08-11 18:18:06 +0545

    Add files via upload

---
+++ main.js
@@ -0,0 +1,82 @@
+
+// var items = document.querySelector('#item');
+
+// parentNode
+
+// console.log(items.parentNode);
+// items.parentNode.style.backgroundColor = '#525f';
+
+// console.log(items.parentNode.parentNode);
+// items.parentNode.parentNode.style.backgroundColor ='green';
+
+// console.log(items.parentNode.parentNode.parentNode);
+// items.parentNode.parentNode.parentNode.style.backgroundColor ='red';
+
+
+// parent element 
+
+// console.log(items.parentElement);
+// console.log(items.parentElement);
+// items.parentElement.style.backgroundColor = 'blue';
+
+// console.log(items.parentElement.parentElement);
+// items.parentElement.parentElement.style.backgroundColor ='pink';
+
+// console.log(items.parentElement.parentElement.parentElement);
+// itemss.parentElement.parentElement.parentElement.style.backgroundColor ='gray';
+
+
+// Child nodes
+// console.log(items.childNodes);
+
+// console.log(items.children);
+// items.children[0].style.color = 'blue';
+// items.children[1].style.color = 'red';
+// items.children[1].innerText = 'hello';
+// items.children[2].style.backgroundColor = 'red';
+
+// // first child
+// console.log(items.firstChild);
+// // first element child
+// console.log(items.firstElementChild);
+// items.firstElementChild.style.backgroundColor='pink';
+// last child
+// console.log(items.lastChild);
+// // last element child
+// console.log(items.lastElementChild);
+
+// next sibling
+// console.log(items.nextSibling);
+// next sibling
+// console.log(items.nextElementSibling);
+
+// // previous sibling
+// console.log(items.previousSibling);
+// // previous sibling
+// console.log(items.previousElementSibling);
+// items.previousElementSibling.innerHTML = 'hii';
+
+
+// // create a new element
+// var nep = document.createElement('div');
+// // add class in div
+// nep.className='hii';
+// // add id in div
+// nep.id='helo';
+// // add attributes
+// nep.setAttribute('title','bye');
+
+// // Add text nodes in the div
+// var nepText = document.createTextNode('helo hiii nothing  just text');
+// // add text with keyword   (new element variable).appendchild.(text node variable)
+// nep.appendChild(nepText);
+
+
+// var container = document.querySelector('header .container');
+// var h1 = document.querySelector('header h1')
+// container.insertBefore(nep,h1);
+
+// console.log(nep);
+
+
+
